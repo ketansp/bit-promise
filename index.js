117dafba4913959268b46fd0955b9c6120394ffa
Ketan Patil <100.percent.ketan@gmail.com>
2022-09-28 00:02:36 +0530

    fixing the logic in promise class implementation

--- index.js
+++ index.js
@@ -14,36 +14,62 @@ class Promise {
      * read .then as a function which registers subsequent success handler function
      * @param {Function} subsequentSuccessFunction 
      */
-    this.then = function(subsequentSuccessFunction){
+    this.then = (subsequentSuccessFunction)=>{
       this.subsequentSuccessFunctions.push(subsequentSuccessFunction);
+      return this;
     }
 
     /**
      * read .catch as a function which registers subsequent error handler function
      * @param {Function} subsequentErrorFunction 
      */
-    this.catch = function(subsequentErrorFunction){
+    this.catch = (subsequentErrorFunction)=>{
       this.subsequentErrorFunctions.push(subsequentErrorFunction);
+      return this;
     }
 
     /**
-     * This is what is trigger all the subsequent success functions
+     * This is what is trigger all the subsequent success function.
+     * The thumb rule is that, if we get another promise in subsequent function, 
+     * we will break current chain to move it to new promise's subsequentSuccessFunctions array.
      */
     let handleSuccess = () => {
       let dataToBePassedOn = [...arguments];
-      this.subsequentSuccessFunctions.forEach(functionElement => {
-        functionElement.apply(null, dataToBePassedOn);
-      });
+      if(this.subsequentSuccessFunctions.length > 0){
+        let targetPromise;
+        for (let index = 0; index < this.subsequentSuccessFunctions.length; index++) {
+          if(targetPromise){
+            targetPromise.then(this.subsequentSuccessFunctions[index])
+          } else {
+            let returnedData = this.subsequentSuccessFunctions[index].apply(null, dataToBePassedOn);
+            if(returnedData instanceof Promise){
+              targetPromise = returnedData;
+            }
+          }
+        }
+      }
     }
 
     /**
-     * This is what is trigger all the subsequent error functions
+     * This is what is trigger all the subsequent error function.
+     * The thumb rule is that, if we get another promise in subsequent error function, 
+     * we will break current chain to move it to new promise's subsequentSuccessFunctions array.
      */
     let handleError = () => {
       let dataToBePassedOn = [...arguments];
-      this.subsequentErrorFunctions.forEach(functionElement => {
-        functionElement.apply(null, dataToBePassedOn);
-      });
+      if(this.subsequentErrorFunctions.length > 0){
+        let targetPromise;
+        for (let index = 0; index < this.subsequentErrorFunctions.length; index++) {
+          if(targetPromise){
+            targetPromise.catch(this.subsequentErrorFunctions[index])
+          } else {
+            let returnedData = this.subsequentErrorFunctions[index].apply(null, dataToBePassedOn);
+            if(returnedData instanceof Promise){
+              targetPromise = returnedData;
+            }
+          }
+        }
+      }
     }
 
     /**
